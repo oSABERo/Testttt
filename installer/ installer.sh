15d0be1c8774b6ac62edd406d591aacc0cb70c04
beer <surakiat.kongchana@enov8.com>
2024-08-17 23:59:00 +0700

    Fix Directory

--- installer/ installer.sh
+++ installer/ installer.sh
@@ -3,12 +3,25 @@
 # Update and install packages
 sudo apt update
 sudo apt upgrade -y
+sudo apt install -y $(cat /Testttt/installer/config/packages.txt)
 sudo apt install python3 python3-pip -y
 
-# Install
-pip3 install -r /Testttt/requirement.txt
+# Create a virtual environment
+python3 -m venv /Testttt/venv
 
-# rsync.service files to systemd directory
+# Activate the virtual environment
+source /Testttt/venv/bin/activate
+
+# Install dependencies
+pip install -r /Testttt/requirement.txt
+
+# Check if the necessary packages are installed
+if ! pip freeze | grep -q 'Error'; then
+    echo "Error: Failed to install"
+    exit 1
+fi
+
+# Copy .service files to systemd directory
 sudo rsync /Testttt/installer/config/backend.service /etc/systemd/system/
 sudo rsync /Testttt/installer/config/data.service /etc/systemd/system/
 
@@ -24,4 +37,3 @@ sudo systemctl start data.service
 # Check service status
 sudo systemctl status backend.service
 sudo systemctl status data.service
-

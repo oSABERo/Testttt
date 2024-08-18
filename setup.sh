e5b4730f23a110b0acd3e3a4fb8b29139471443a
beer <surakiat.kongchana@enov8.com>
2024-08-18 22:18:56 +0700

    Fix overall

--- setup.sh
+++ setup.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
+# check
+if [ "$EUID" -ne 0 ]; then
+    echo "Error: Failed to install"
+    exit 1
+fi
+
 # Update and installcpackages
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y $(cat installer/config/packages.txt)
-sudo apt install python3 python3-pip python3-venv -y
+apt update
+apt install -y $(cat installer/config/packages.txt)
 
 # Createcenvironment
-rm -rf venv
-python3 -m venv venv
+rm -rf venv #todo
+python3 -m venv venv #change name venv
 
 # Activatecnvironment
 source venv/bin/activate
 
 # Installcrequirements.txt
 pip install -r requirements.txt
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to install"
-    exit 1
-fi
 
-sudo rsync installer/config/backend.service /etc/systemd/system/
-sudo rsync installer/config/data.service /etc/systemd/system/
-sudo rsync installer/config/subbackend.service /etc/systemd/system/
+rsync installer/config/backend.service /etc/systemd/system/
+# sudo rsync installer/config/data.service /etc/systemd/system/ 
+# sudo rsync installer/config/subbackend.service /etc/systemd/system/ 
 
 # Reload and enable services
 #sudo systemctl daemon-reload
-sudo systemctl enable data.service
-sudo systemctl enable backend.service
+# sudo systemctl enable data.service
+systemctl enable backend.service
 
-# Start service
-sudo systemctl start data.service
-sudo systemctl start backend.service
+# reStart service
+# sudo systemctl restart data.service
+systemctl restart backend.service
 
-# Show Status 
-sudo systemctl status data.service
